fe8471fe2bf66c5997126b1e9cf01c1361be4a2b
vins-stha <shrs_samar@yahoo.com>
2022-03-02 15:35:06 +0200

    updated model and views

--- products_cart/models.py
+++ products_cart/models.py
@@ -1,3 +1,58 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(blank=False, max_length=200)
+    created_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-name"]
+
+
+
+"""
+Model class for Product
+"""
+class Product(models.Model):
+
+    colors = (
+        ('red','Red'),
+        ('blue', 'Blue'),
+        ('black', 'Black'),
+        ('white', 'White'),
+        ('grey', 'Silver')
+    )
+
+    inStock = (("yes","Yes"), ("no", "No"),("limited","Limited (< 10 )"))
+    title = models.CharField(blank=False,max_length=200)
+    description = models.TextField(blank=True,max_length=1000)
+    price = models.DecimalField(blank=False, max_digits=10, decimal_places=2)
+    cat_id = models.ForeignKey(Category,on_delete=models.PROTECT)
+    colors = models.CharField(max_length=20, choices=colors, default="black")
+    available = models.IntegerField(default=0)
+    inStock = models.CharField(default="No",choices=inStock, max_length=20)
+    added_on = models.DateField(auto_now=True)
+    barcode = models.IntegerField(blank=False, default=1234567890)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ("-title","price","-added_on")
+
+
+"""
+Customers Model 
+"""
+class Customer(models.Model):
+    pass
+
+
+"""
+Cart Model 
+"""
+class Cart(models.Model):
+    pass
--- shopping_cart_core/settings.py
+++ shopping_cart_core/settings.py
@@ -37,9 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'rest_framework',
+    'products_cart',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -50,7 +55,9 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'shopping_cart_core.urls'
-
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
